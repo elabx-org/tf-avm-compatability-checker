a30eeed78fa7ce2862b1988ac4ff423ef1c9cb33
elmerfds <22656503+elmerfds@users.noreply.github.com>
2024-10-04 22:58:29 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
 module "test_module" {
   source  = "AVM_SOURCE"
+  # Add any required variables here, e.g.:
+  # resource_group_name = "example-rg"
+  # location            = "eastus"
 }
