d7acaad6989c322087ef8afdc5d2bf1cbe1ac2ce
elmerfds <22656503+elmerfds@users.noreply.github.com>
2024-10-05 17:43:10 +0100

    Create main.tf

---
+++ terraform/normal_config/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "AZURERM_VERSION"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "test_module" {
+  source = "AVM_SOURCE"
+  # Add any required variables here
+}
